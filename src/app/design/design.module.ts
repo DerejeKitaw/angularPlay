35fc4af6105fa91e4a918d1738cf044834497d21
Dereje Kitaw <dkitaw@solareworld.local>
2017-11-01 13:44:54 -0400

    design module added

---
+++ src/app/design/design.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DesignRoutingModule } from './design-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DesignRoutingModule
+  ],
+  declarations: []
+})
+export class DesignModule { }

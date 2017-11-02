8b2aba840c3e69b8801c199c09f4e81903766523
Dereje Kitaw <dkitaw@solareworld.local>
2017-11-02 17:18:46 -0400

    form added in component 2-b

--- src/app/design/comp2/comp2.module.ts
+++ src/app/design/comp2/comp2.module.ts
@@ -5,12 +5,16 @@ import { Comp2RoutingModule } from './comp2-routing.module';
 import { Comp2AComponent } from './comp2-a/comp2-a.component';
 import { Comp2BComponent } from './comp2-b/comp2-b.component';
 import { Comp2CComponent } from './comp2-c/comp2-c.component';
+import { HeroService } from 'app/design/comp2/comp2-b/hero.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    Comp2RoutingModule
+    Comp2RoutingModule,
+    ReactiveFormsModule
   ],
-  declarations: [Comp2AComponent, Comp2BComponent, Comp2CComponent]
+  declarations: [Comp2AComponent, Comp2BComponent, Comp2CComponent],
+  providers: [ HeroService ], // <-- #4 provide HeroService
 })
 export class Comp2Module { }

de75dc9a763192dc5cb0d76c0c43ac7252eb1ceb
Dereje Kitaw <dkitaw@solareworld.local>
2017-11-03 16:59:17 -0400

    comp2 added

--- src/app/design/comp2/comp2-c/comp2-c.component.ts
+++ src/app/design/comp2/comp2-c/comp2-c.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-comp2-c',
   templateUrl: './comp2-c.component.html',
   styleUrls: ['./comp2-c.component.scss']
 })
-export class Comp2CComponent implements OnInit {
+export class Comp2CComponent  {
+  elRef;
+  constructor(elRef: ElementRef) { 
+    this.elRef=elRef;
+    console.log(this.elRef.nativeElement);
 
-  constructor() { }
+  }
 
   ngOnInit() {
+  }
+  move(ref: ElementRef) {
+    console.log(this.elRef.nativeElement);
   }
 
 }
